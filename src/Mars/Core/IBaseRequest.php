804af9f1f7e955c4a2e1fc51c0f1de735e8c70f3
Hassane Dao <dao.hassane@gmail.com>
2020-12-11 15:55:18 +0000

    feat(interface): request and response

---
+++ src/Mars/Core/IBaseRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Mars\Core;
+
+
+interface IBaseRequest
+{
+    /**
+     * @return mixed
+     */
+    public function getPath();
+
+    /**
+     * @return string
+     */
+    public function method(): string;
+
+    /**
+     * @return bool
+     */
+    public function isGet(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isPost(): bool;
+
+    /**
+     * @return array
+     */
+    public function getBody() : array;
+}
